2dc66e1b84424ab0672a9c1b126cf23a674e0bd6
Lemmon <lemmon@users.noreply.github.com>
2018-08-06 16:07:19 +0200

    errors: catching and reporting

--- src/backend/lib/debug.php
+++ src/backend/lib/debug.php
@@ -6,3 +6,30 @@ function dump($data)
 {
   echo '<pre>' . print_r($data, TRUE) . '</pre>';
 }
+
+function error(int $code, string $message = NULL): void
+{
+  http_response_code($code);
+  json([
+    'error' => TRUE,
+    'message' => $message,
+  ]);
+  exit(1);
+}
+
+ini_set('display_errors', 0);
+//error_reporting(0);
+
+set_error_handler(function ($errno, $errstr) {
+  die('_ERROR');
+}, E_ALL);
+
+set_exception_handler(function (\Throwable $e) {
+  error(500, $e->getMessage());
+});
+
+register_shutdown_function(function () {
+  if ($err = error_get_last()) {
+    error(500, $err['message']);
+  }
+});
--- src/backend/lib/http.php
+++ src/backend/lib/http.php
@@ -7,12 +7,3 @@ define('HTTP', [
   'method' => $_SERVER['REQUEST_METHOD'] ?? 'GET',
   'path'   => $_SERVER['PATH_INFO'] ?? '/',
 ]);
-
-function error(int $code): void
-{
-  http_response_code($code);
-  json([
-    'error' => TRUE,
-  ]);
-  exit(1);
-}

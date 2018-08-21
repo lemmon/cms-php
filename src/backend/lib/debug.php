756aca89c099753459c23bf3f23a184ae768a565
Lemmon <lemmon@users.noreply.github.com>
2018-08-21 20:29:33 +0200

    error reporting: file and line number

--- src/backend/lib/debug.php
+++ src/backend/lib/debug.php
@@ -7,25 +7,35 @@ function dump($data)
   echo '<pre>' . print_r($data, TRUE) . '</pre>';
 }
 
-function error(int $code, string $message = NULL): void
+function error(int $code, $error = NULL): void
 {
   http_response_code($code);
-  json([
+  json(array_filter(is_array($error) ? array_replace([
     'error' => TRUE,
-    'message' => $message,
-  ]);
+  ], $error) : [
+    'error' => TRUE,
+    'message' => $error,
+  ]));
   exit(1);
 }
 
 ini_set('display_errors', 0);
 //error_reporting(0);
 
-set_error_handler(function ($errno, $errstr) {
-  error(500, $errstr);
+set_error_handler(function ($errno, $errstr, $errfile, $errline) {
+  error(500, [
+    'message' => $errstr,
+    'file' => $errfile,
+    'line' => $errline,
+  ]);
 }, E_ALL);
 
 set_exception_handler(function (\Throwable $e) {
-  error(500, $e->getMessage());
+  error(500, [
+    'message' => $e->getMessage(),
+    'file' => $e->getFile(),
+    'line' => $e->getLine(),
+  ]);
 });
 
 register_shutdown_function(function () {

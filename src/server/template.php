d6658a7c01f6e2e49173ff03fa97a475be36716e
Lemmon <lemmon@users.noreply.github.com>
2018-09-01 09:44:51 +0200

    abrusco 0.7.0

--- src/server/template.php
+++ src/server/template.php
@@ -5,28 +5,11 @@
   <title>Garner</title>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
   <link rel="stylesheet" href="<?= $root ?>/garner.<?= $build ?>.css">
-  <link rel="stylesheet" href="https://unpkg.com/abrusco@0.6.4/css/abrusco.min.css">
+  <link rel="stylesheet" href="https://unpkg.com/abrusco@0.7.0/css/abrusco.min.css">
   <script>
   window.rootPath = '<?= $root ?>';
   </script>
   <script src="<?= $root ?>/garner.<?= $build ?>.js" defer></script>
-  <style>
-  :root {
-    --aqua:    #7fdbff;
-    --blue:    #0074d9;
-    --navy:    #001f3f;
-    --teal:    #39cccc;
-    --green:   #2ecc40;
-    --olive:   #3d9970;
-    --lime:    #01ff70;
-    --yellow:  #ffdc00;
-    --orange:  #ff851b;
-    --red:     #ff4136;
-    --fuchsia: #f012be;
-    --purple:  #b10dc9;
-    --maroon:  #85144b;
-  }
-  </style>
 </head>
 <body class="col justify-center items-center">
 

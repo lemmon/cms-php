3c75b06ef5e9cacf50cbc92d7de8550b5f658609
Lemmon <lemmon@users.noreply.github.com>
2018-09-15 09:57:10 +0200

    number field: increase, decrease

--- src/app/views/components/field-number.js
+++ src/app/views/components/field-number.js
@@ -28,16 +28,34 @@ module.exports = class NumberField extends Field {
         <div class="p025 ac" style="width: 2rem;">
           <button
             type="button"
-            class="button button-clear lh4 color-black-40"
+            class="button button-transparent lh4 color-black-20"
+            onclick=${e => {
+              this.increase(1, 1)
+            }}
+            onmousedown=${e => {
+              e.preventDefault()
+              this.sanitize()
+              this.element.querySelector('input').focus()
+            }}
             onfocus=${e => this.handleFocus(e)}
             onblur=${e => this.handleBlur(e)}
-          >\u2191</button>
+            tabindex="-1"
+          ><span class="button-caption color-black-50">\u2191</span></button>
           <button
             type="button"
-            class="button button-clear lh4 color-black-40"
+            class="button button-transparent lh4 color-black-20"
+            onclick=${e => {
+              this.increase(-1, 0)
+            }}
+            onmousedown=${e => {
+              e.preventDefault()
+              this.sanitize()
+              this.element.querySelector('input').focus()
+            }}
             onfocus=${e => this.handleFocus(e)}
             onblur=${e => this.handleBlur(e)}
-          >\u2193</button>
+            tabindex="-1"
+          ><span class="button-caption color-black-50">\u2193</span></button>
         </div>
       </div>
     `, {
@@ -45,9 +63,25 @@ module.exports = class NumberField extends Field {
     })
   }
 
+  increase(inc = 1, def = 1) {
+    this.value = Number.isInteger(this.value) ? this.value + inc : def
+    this.state.inputValue = null
+    this.state.touched = true
+    this.element.querySelector('input').value = this.value
+    // errors
+    if (this.state.errors) {
+      this.state.errors = null
+      this.element.classList.remove('field-invalid')
+    }
+    // onchange event
+    if (this.props.onchange) {
+      this.props.onchange(this)
+    }
+  }
+
   sanitize() {
     return super.sanitize(input => {
-      const res = input.match(/^[\s\d]+$/) && parseInt(input.replace(/\s+/g, ''))
+      const res = input.match(/^\s*\-?[\s\d]+$/) && parseInt(input.replace(/\s+/g, ''))
       return Number.isInteger(res) ? res : input.trim() || null
     })
   }
